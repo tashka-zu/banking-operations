13a5d979470c81763c6f15b52bcb10f16f3d18d0
unknown <zubkovanataska21@gmail.com>
2025-01-28 15:50:06 +0300

    add 2 function

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,6 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """Функция, которая маскирует номера банковской карты"""
     card_number_str = str(card_number)
-    if len(card_number_str) != 16:
-        raise ValueError
     card_mask = card_number_str[:6] + "******" + card_number_str[-4:]
     formatted_card_mask = " ".join(
         card_mask[i : i + 4] for i in range(0, len(card_mask), 4)
@@ -10,16 +8,9 @@ def get_mask_card_number(card_number: int) -> str:
     return formatted_card_mask
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: str) -> str:
     """Функция, которая маскирует номера банковского счета"""
     account_number_str = str(account_number)
-    if len(account_number_str) != 20:
-        raise ValueError
     account_mask = "**" + account_number_str[-4:]
     return account_mask
 
-# использование функции
-card_number = int(input())
-account_number = int(input())
-print(get_mask_card_number(card_number))
-print(get_mask_account(account_number))
--- src/widget.py
+++ src/widget.py
@@ -1,17 +1,28 @@
-from masks import get_mask_account
-from masks import get_mask_card_number
-
+from masks import get_mask_account, get_mask_card_number
+from datetime import datetime
 
 def mask_account_card(info_account_or_card: str) -> str:
     """Обработка информации о о картах и счетах"""
-    if "Счёт" or "Счет" in info_account_or_card:
-        account_number = info_account_or_card[-1]
+    if "Счет" in info_account_or_card:
+        account_split = info_account_or_card.split()
+        account_number = account_split[-1]
         masked_number = get_mask_account(account_number)
         return info_account_or_card.replace(account_number, masked_number)
     else:
-        card_number = info_account_or_card[-1]
+        card_split = info_account_or_card.split()
+        card_number = card_split[-1]
         masked_number = get_mask_card_number(card_number)
         return info_account_or_card.replace(card_number, masked_number)
 
-info_account_or_card = str(input())
-print(mask_account_card(info_account_or_card))
+
+def get_date(date_str: str) -> str:
+    """Функция, которая переделывает входную строку в ДД.ММ.ГГГГ"""
+    date_obj = datetime.fromisoformat(date_str)
+    formatted_date = date_obj.strftime("%d.%m.%Y")
+    return formatted_date
+
+
+
+if __name__ == "__main__":
+    print(mask_account_card(input()))
+    print(get_date(input()))
