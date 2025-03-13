5e4ed0dbcaf6c2d8d2059f7739b890acf6ecf90b
tashaa-zu <zubkovanaraska21@gmail.com>
2025-03-13 10:54:18 +0300

    add logging for 2 moduls

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,30 @@
+import logging
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler("../logs/masks.log")
+file_handler.setLevel(logging.DEBUG)
+
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция, которая маскирует номера банковской карты"""
     card_number_str = str(card_number)
     if card_number_str == "":
+        logger.error("Пустой ввод данных для номера карты")
         return ""
     if not card_number_str.isdigit():
+        logger.error("Некорректный ввод данных для номера карты")
         return "Некорректный ввод данных"
     else:
         card_mask = card_number_str[:6] + "******" + card_number_str[-4:]
         formatted_card_mask = " ".join(card_mask[i : i + 4] for i in range(0, len(card_mask), 4))
+        logger.info(f"Номер карты успешно замаскирован: {formatted_card_mask}")
         return formatted_card_mask
 
 
@@ -15,9 +32,16 @@ def get_mask_account(account_number: str) -> str:
     """Функция, которая маскирует номера банковского счета"""
     account_number_str = str(account_number)
     if account_number_str == "":
+        logger.error("Пустой ввод данных для номера счета")
         return ""
     if not account_number_str.isdigit():
+        logger.error("Некорректный ввод данных для номера счета")
         return "Некорректный ввод данных"
     else:
         account_mask = "**" + account_number_str[-4:]
+        logger.info(f"Номер счета успешно замаскирован: {account_mask}")
         return account_mask
+
+
+get_mask_card_number("1234567890123456")
+get_mask_account("1234567890")
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,38 @@
 import json
+import logging
 import os
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler("../logs/utils.log")
+file_handler.setLevel(logging.DEBUG)
+
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(file_handler)
 
 
 def load_transactions(file_path):
     """Загружает данные о финансовых транзакциях из JSON-файла."""
     if not os.path.exists(file_path):
+        logger.error(f"Файл {file_path} не найден.")
         return []
 
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
             if data:
+                logger.info(f"Данные успешно загружены из файла {file_path}.")
                 return data
             else:
+                logger.error(f"Файл {file_path} пуст или не содержит данных.")
                 return []
-    except (json.JSONDecodeError, IOError):
+    except (json.JSONDecodeError, IOError) as e:
+        logger.error(f"Ошибка при чтении файла {file_path}: {e}")
         return []
+
+
+file_path = "../data/operations.json"
+transactions = load_transactions(file_path)
